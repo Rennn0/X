5a599bf45422f8cfc1cee3ae5ae3b011669855da
Luka Danelia <lukadanelia056@gmail.com>
2023-07-08 02:58:44 +0400

    <>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MainService } from 'src/lib/services/main.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'X';
+  constructor(private main: MainService) {
+    main.log('Hi');
+  }
 }
